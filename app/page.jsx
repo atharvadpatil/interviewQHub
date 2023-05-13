32ebc045051085a35acc62e1e5f785e84d188a57
atharvadpatil <atharva.dp@somaiya.edu>
2023-05-13 23:37:59 +0530

    components created

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,18 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover and Share
+      <br />
+      <span className="orange_gradient text-center">Interview Questions</span>
+      </h1>
+      <p className="desc text-center">
+      InterviewQHub is a platform dedicated to the sharing and exchange of interview questions. It provides a community-driven space where job seekers, interviewees, and hiring managers can contribute and access a wide range of interview questions from various industries and job roles. <br></br>GO GET YOUR DREAM JOB!!!!!!
+      </p>
+
+      <Feed />
+    </section>
   )
 }
 
