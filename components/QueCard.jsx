a0e6f5a3e777a34223729f3a0de80be1e6cb1b7f
atharvadpatil <atharva.dp@somaiya.edu>
2023-05-15 23:32:40 +0530

    profile page + edit and delete question

--- components/QueCard.jsx
+++ components/QueCard.jsx
@@ -7,7 +7,9 @@ import { usePathname, useRouter } from "next/navigation";
 
 
 export default function QueCard({ post, handleEdit, handleDelete, handleTagClick }) {
-
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   const [copied, setCopied] = useState("");
 
@@ -63,6 +65,23 @@ export default function QueCard({ post, handleEdit, handleDelete, handleTagClick
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
